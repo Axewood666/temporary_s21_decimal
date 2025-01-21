13fb0fc631c7aec19f9ad410504e941d520a4d9d
Tytosboo <tytosboo@student.21-school.ru>
2025-01-21 15:49:30 +0300

    add test for add

--- decimal/main.c
+++ decimal/main.c
@@ -1,10 +1,10 @@
 #include "s21_decimal.h"
 
 int main() {
-  s21_decimal a = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x80000000}};
-  s21_decimal b = {{0x00000011, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal num1 = {{0x00001234, 0x00000000, 0x00000000, 0x00020000}};
+  s21_decimal num2 = {{0x00005678, 0x00000000, 0x00000000, 0x00030000}};
   s21_decimal res;
-  printf("%d\n", s21_sub(a, b, &res));
+  printf("%d\n", s21_add(num1, num2, &res));
   printf("|%8.8X| |%8.8X| |%8.8X| |%8.8X|\n", res.bits[0], res.bits[1],
          res.bits[2], res.bits[3]);
   // printf("|%8u| |%8X| |%8X| |%8X|\n",b.bits[0], res.bits[1], res.bits[2],
--- decimal/tests/arithmetic_tests.c
+++ decimal/tests/arithmetic_tests.c
@@ -1,18 +1,95 @@
-#include "../s21_decimal.h"
 #include <check.h>
 
-START_TEST(test_test)
-{
-  ck_assert_int_eq(1, 1);
+#include "../s21_decimal.h"
+
+START_TEST(add_int_both_positive) {
+  s21_decimal num1 = {{0x00000010, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal num2 = {{0x00000020, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res1 = {{0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res2 = {{0x00000030, 0x00000000, 0x00000000, 0x00000000}};
+  ck_assert_int_eq(s21_add(num1, num2, &res1), 0);
+  ck_assert_int_eq(res1.bits[0], res2.bits[0]);
+  ck_assert_int_eq(res1.bits[1], res2.bits[1]);
+  ck_assert_int_eq(res1.bits[2], res2.bits[2]);
+  ck_assert_int_eq(res1.bits[3], res2.bits[3]);
 }
 END_TEST
 
-Suite *test_arithmetic(void)
-{
+START_TEST(add_int_both_negative) {
+  s21_decimal num1 = {{0x00000010, 0x00000000, 0x00000000, 0x80000000}};
+  s21_decimal num2 = {{0x00000020, 0x00000000, 0x00000000, 0x80000000}};
+  s21_decimal res1 = {{0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res2 = {{0x00000030, 0x00000000, 0x00000000, 0x80000000}};
+  ck_assert_int_eq(s21_add(num1, num2, &res1), 0);
+  ck_assert_int_eq(res1.bits[0], res2.bits[0]);
+  ck_assert_int_eq(res1.bits[1], res2.bits[1]);
+  ck_assert_int_eq(res1.bits[2], res2.bits[2]);
+  ck_assert_int_eq(res1.bits[3], res2.bits[3]);
+}
+END_TEST
+
+START_TEST(add_int_first_positive_second_negative) {
+  s21_decimal num1 = {{0x00000010, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal num2 = {{0x00000020, 0x00000000, 0x00000000, 0x80000000}};
+  s21_decimal res1 = {{0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res2 = {{0x00000010, 0x00000000, 0x00000000, 0x80000000}};
+  ck_assert_int_eq(s21_add(num1, num2, &res1), 0);
+  ck_assert_int_eq(res1.bits[0], res2.bits[0]);
+  ck_assert_int_eq(res1.bits[1], res2.bits[1]);
+  ck_assert_int_eq(res1.bits[2], res2.bits[2]);
+  ck_assert_int_eq(res1.bits[3], res2.bits[3]);
+}
+END_TEST
+
+START_TEST(add_int_first_negative_second_positive) {
+  s21_decimal num1 = {{0x00000010, 0x00000000, 0x00000000, 0x80000000}};
+  s21_decimal num2 = {{0x00000020, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res1 = {{0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res2 = {{0x00000010, 0x00000000, 0x00000000, 0x80000000}};
+  ck_assert_int_eq(s21_add(num1, num2, &res1), 0);
+  ck_assert_int_eq(res1.bits[0], res2.bits[0]);
+  ck_assert_int_eq(res1.bits[1], res2.bits[1]);
+  ck_assert_int_eq(res1.bits[2], res2.bits[2]);
+  ck_assert_int_eq(res1.bits[3], res2.bits[3]);
+}
+END_TEST
+
+START_TEST(add_not_int_both_positive) {
+  s21_decimal num1 = {{0x000004D2, 0x00000000, 0x00000000, 0x00020000}};
+  s21_decimal num2 = {{0x0000162E, 0x00000000, 0x00000000, 0x00030000}};
+  s21_decimal res1 = {{0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res2 = {{0x00004662, 0x00000000, 0x00000000, 0x00030000}};
+  ck_assert_int_eq(s21_add(num1, num2, &res1), 0);
+  ck_assert_int_eq(res1.bits[0], res2.bits[0]);
+  ck_assert_int_eq(res1.bits[1], res2.bits[1]);
+  ck_assert_int_eq(res1.bits[2], res2.bits[2]);
+  ck_assert_int_eq(res1.bits[3], res2.bits[3]);
+}
+END_TEST
+
+START_TEST(add_not_int_both_negative) {
+  s21_decimal num1 = {{0x000004D2, 0x00000000, 0x00000000, 0x80020000}};
+  s21_decimal num2 = {{0x0000162E, 0x00000000, 0x00000000, 0x80030000}};
+  s21_decimal res1 = {{0x00000000, 0x00000000, 0x00000000, 0x00000000}};
+  s21_decimal res2 = {{0x00004662, 0x00000000, 0x00000000, 0x80030000}};
+  ck_assert_int_eq(s21_add(num1, num2, &res1), 0);
+  ck_assert_int_eq(res1.bits[0], res2.bits[0]);
+  ck_assert_int_eq(res1.bits[1], res2.bits[1]);
+  ck_assert_int_eq(res1.bits[2], res2.bits[2]);
+  ck_assert_int_eq(res1.bits[3], res2.bits[3]);
+}
+END_TEST
+
+Suite *test_arithmetic(void) {
   Suite *s = suite_create("Arithmetic test");
   TCase *tc = tcase_create("Tests");
 
-  tcase_add_test(tc, test_test);
+  tcase_add_test(tc, add_int_both_positive);
+  tcase_add_test(tc, add_int_both_negative);
+  tcase_add_test(tc, add_int_first_positive_second_negative);
+  tcase_add_test(tc, add_int_first_negative_second_positive);
+  tcase_add_test(tc, add_not_int_both_positive);
+  tcase_add_test(tc, add_not_int_both_negative);
 
   suite_add_tcase(s, tc);
   return s;
