5d40c022d6a4a4efa75bfc86250bc3b9f32b812a
austinma <austinma@student.21-school.ru>
2025-02-25 12:56:00 +0300

    это сделал Данис, но я просто запушил

--- s21_decimal/convertors/from_decimal_to_int.c
+++ s21_decimal/convertors/from_decimal_to_int.c
@@ -2,17 +2,18 @@
 
 int s21_from_decimal_to_int(s21_decimal src, int *dst) {
     int res = 1;
-    
-    if(dst){
-        s21_decimal src_int_part = create_zero_decimal();
-        s21_truncate(src,&src_int_part);
-        if(src_int_part.bits[0] == 0 && src_int_part.bits[1] == 0){
-            *dst = src_int_part.bits[0];
-            if (get_sign(src)) {
-                *dst *= -1;
-            }
-            res = 0;
+    s21_decimal temp = {{0, 0, 0, 0}};
+    s21_truncate(src, &temp);
+    if (dst && temp.bits[1] == 0 && temp.bits[2] == 0) {
+      long long value = temp.bits[0];
+      int sign = get_sign(src);
+      if (value <= 2147483647) {
+        *dst = (int)value;
+        if (sign) {
+          *dst = -(*dst);
         }
+        res = 0;
+      }
     }
     return res;
   }
