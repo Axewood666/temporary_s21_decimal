0cc184bde0992fdc8e7cded997f89b0888942012
austinma <austinma@student.21-school.ru>
2025-02-26 22:30:12 +0300

    fixed round

--- s21_decimal/other_funcs/round.c
+++ s21_decimal/other_funcs/round.c
@@ -3,17 +3,15 @@
 int s21_round(s21_decimal value, s21_decimal *result) {
     int status = 0;
     if (result) {
-      s21_decimal int_part = {{0, 0, 0, 0}};
-      s21_decimal one = {{1, 0, 0, 0}};
-      s21_decimal zero = {{0, 0, 0, 0}};
-      s21_truncate(value, &int_part);
-      if (get_first_digit_after_decimal(value) >= 5) {
-        s21_decimal rounded = {{0, 0, 0, 0}};
-        status = s21_add(int_part, one, &rounded);
-        *result = (status == 0) ? rounded : zero;
-      } else {
-        *result = int_part;
-      }
+      int sign = get_sign(value);
+      s21_decimal frac;
+      s21_decimal val_uns_trun;
+      s21_decimal val_uns = s21_decimal_abs(value);
+      s21_truncate(val_uns,&val_uns_trun);
+      s21_sub(val_uns,val_uns_trun,&frac);
+      val_uns_trun = s21_round_bank(val_uns_trun,frac);
+      *result = val_uns_trun;
+      set_sign(result,sign);
     } else {
       status = 1;
     }
