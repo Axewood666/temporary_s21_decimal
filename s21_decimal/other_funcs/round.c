eb14ac2f49ae73a544a3c52316cdd5fd5d5101ba
Axewood <igor.zhugar@mail.ru>
2025-03-16 17:57:32 +0300

    other func style refactoring

--- s21_decimal/other_funcs/round.c
+++ s21_decimal/other_funcs/round.c
@@ -1,19 +1,21 @@
 #include "../s21_decimal.h"
 
 int s21_round(s21_decimal value, s21_decimal *result) {
-    int status = 0;
-    if (result) {
-      int sign = get_sign(value);
-      s21_decimal frac;
-      s21_decimal val_uns_trun;
-      s21_decimal val_uns = s21_decimal_abs(value);
-      s21_truncate(val_uns,&val_uns_trun);
-      s21_sub(val_uns,val_uns_trun,&frac);
-      val_uns_trun = s21_round_bank(val_uns_trun,frac);
-      *result = val_uns_trun;
-      set_sign(result,sign);
-    } else {
-      status = 1;
+    if (!result) {
+        return 1;
     }
+    int status = 0;
+
+    int sign = get_sign(value);
+
+    s21_decimal frac;
+    s21_decimal val_uns_trun;
+    s21_decimal val_uns = s21_decimal_abs(value);
+    s21_truncate(val_uns, &val_uns_trun);
+    s21_sub(val_uns, val_uns_trun, &frac);
+    val_uns_trun = s21_round_bank(val_uns_trun, frac);
+    
+    *result = val_uns_trun;
+    set_sign(result, sign);
     return status;
-  }
+}
